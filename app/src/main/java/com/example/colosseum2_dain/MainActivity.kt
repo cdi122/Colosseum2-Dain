cd1d19e6547d8eb714cdbf4be3d1ec380f1853de
DI <cdi122@nate.com>
2022-09-22 16:22:59 +0900

    로그인 버튼이 눌리면 입력한 아이디/ 비번가져오기

--- app/src/main/java/com/example/colosseum2_dain/MainActivity.kt
+++ app/src/main/java/com/example/colosseum2_dain/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum2_dain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        loginBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+
+        }
     }
 
     override fun setValues() {
