7418ac99835232ed86ed593fbacfda7d6a54ce33
DI <cdi122@nate.com>
2022-09-23 17:28:35 +0900

    회언가입 성공 시 닉네임 추출해서 토스트로 추출

--- app/src/main/java/com/example/colosseum2_dain/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum2_dain/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum2_dain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.colosseum2_dain.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -24,15 +25,31 @@ class SignUpActivity : BaseActivity() {
             ServerUtil.putRequestSignUp(inputEmail,inputPw,inputNickname, object : ServerUtil.Companion.JsonResponseHandler{
                 override fun onResponse(jsonObj: JSONObject) {
 
+                    val code = jsonObj.getInt("code")
+                    if (code == 200){
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickName = userObj.getString("nick_name")
+                        runOnUiThread { Toast.makeText(mContext, "${nickName}님, 환영합니다!", Toast.LENGTH_SHORT).show() }
+                        finish()}
+
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
+                        }
+
+                    }
                 }
 
-            })
+            }
+                )
 
         }
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+
     }
 
 
