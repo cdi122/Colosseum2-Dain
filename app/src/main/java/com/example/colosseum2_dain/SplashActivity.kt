9207b60d3ee9d9f572dbdbc75d1836884be0d481
DI <cdi122@nate.com>
2022-09-30 15:45:57 +0900

    간단한 자동로그인 마무리

--- app/src/main/java/com/example/colosseum2_dain/SplashActivity.kt
+++ app/src/main/java/com/example/colosseum2_dain/SplashActivity.kt
@@ -1,7 +1,12 @@
 package com.example.colosseum2_dain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.example.colosseum2_dain.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,20 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            if (ContextUtil.getToken(mContext) == ""){
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+            else{
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)}
+
+            finish()
+
+        }, 3000)
     }
 
 
